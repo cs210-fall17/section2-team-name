5efcc409731abe57c0f51ab88d6c788e0f1fb272
Ian Valentine <ivalentine@email.arizona.edu>
2017-09-11 11:39:05 -0700

    conflict

--- Hello.java
+++ Hello.java
@@ -10,7 +10,9 @@ public class Hello {
 
     public static void callSign() {
         
-        // Red Leader should add a print statement that says "Lock S-foils in attack position." 
+        system.out.println("Lock S-foils in attack position."):
+	system.out.println("Stabilize your rear deflectors... Watch for enemy fighters."):
+// Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
     }
